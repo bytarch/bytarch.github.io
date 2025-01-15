c846e15138e804f6d56c352cc9b835f604ef2c01
bytarch <159407182+bytarch@users.noreply.github.com>
2025-01-15 11:00:07 -0600

    Update session.js

--- cdn/js/session.js
+++ cdn/js/session.js
@@ -3,24 +3,8 @@
 // version: 2.0
 (async function () {
 
-  function getSessionTokenFromCookie(cookieName) {
-    // Split cookies string into individual cookies
-    const cookiesArray = document.cookie.split(';');
-    
-    // Loop through cookies to find the one with the specified name
-    for (let cookie of cookiesArray) {
-        // Remove leading and trailing spaces from cookie string
-        cookie = cookie.trim();
-        
-        // Check if the cookie starts with the specified cookieName
-        if (cookie.startsWith(cookieName + '=')) {
-            // Extract the sessionToken from the cookie
-            return cookie.substring(cookieName.length + 1);
-        }
-    }
-    
-    // Return null if the cookie with the specified name is not found
-    return null;
+  function getSessionTokenFromLocalStorage() {
+    return localStorage.getItem('auth.sessionToken');
   }
 
   // Function to check if URL includes "login", "signup", or "pricing"
@@ -32,32 +16,21 @@
       currentUrl.includes('pricing')
     );
   }
-  
 
-  function handleLogout()  {
-  // Clear the session token from localStorage
-  handle_sessionRemove();
-
+  function handleLogout() {
+    // Clear the session token from localStorage
+    handle_sessionRemove();
 
     const currentUrl = window.location.href;
-            window.location.href = `https://blue-lion-79.telebit.io/login?callbackUrl=${encodeURIComponent(
-              currentUrl
-            )}`;
-  
-  }; 
-  
-  
-  function handle_sessionRemove()  {
+    window.location.href = `https://blue-lion-79.telebit.io/login?callbackUrl=${encodeURIComponent(
+      currentUrl
+    )}`;
+  }
+
+  function handle_sessionRemove() {
     // Clear the session token from localStorage
     localStorage.removeItem('sessionToken');
-    
     localStorage.removeItem('auth.sessionToken');
-  
-  
-    }; 
-  // Function to retrieve session token from localStorage
-  function getSessionTokenFromLocalStorage() {
-    return localStorage.getItem('auth.sessionToken');
   }
 
   // Function to parse session token from query parameters
@@ -74,8 +47,8 @@
   }
 
   // Function to set session token in localStorage
-  function setSessionTokenToLocalStorage(cookieName, sessionToken) {
-    localStorage.setItem(cookieName, sessionToken);
+  function setSessionTokenToLocalStorage(sessionToken) {
+    localStorage.setItem('auth.sessionToken', sessionToken);
   }
 
   // Function to fetch session data and manage redirects
@@ -85,27 +58,26 @@
       return;
     }
 
-    // Retrieve the sessionToken from cookie first
-    let sessionToken = getSessionTokenFromCookie('auth.sessionToken');
+    // Retrieve the sessionToken from localStorage
+    let sessionToken = getSessionTokenFromLocalStorage();
 
-    // If session token is not found in cookie, check query parameters
+    // If session token is not found in localStorage, check query parameters
     if (!sessionToken) {
       sessionToken = parseSessionTokenFromQueryParams();
     }
 
-    
     // If session token is found, proceed with fetching session data
     if (sessionToken) {
       try {
         let headers = {};
-        
-        // If session token is retrieved from localStorage or query params, include Authorization header
-        if (sessionToken !== getSessionTokenFromCookie('auth.sessionToken')) {
+
+        // If session token is retrieved from query params, include Authorization header
+        if (sessionToken !== getSessionTokenFromLocalStorage()) {
           headers = {
             'Authorization': `Bearer ${sessionToken}` // Include session token in Authorization header
           };
         }
-        
+
         const response = await fetch('https://blue-lion-79.telebit.io/api/auth/session', {
           method: 'GET',
           headers: headers
@@ -120,7 +92,7 @@
         // Check if the response contains a new session token
         if (data && data.accessToken) {
           const newSessionToken = data.accessToken;
-         
+
           // Check if expiration_date is less than the current date
           const expirationDate = new Date(data.expiration_date);
           const currentDate = new Date();
@@ -135,21 +107,17 @@
         }
       } catch (error) {
         handleLogout();
-   
+
         // Redirect to the login page on fetch error
-    
         console.error('There was a problem with the fetch operation:', error);
       }
     } else {
-     
       handleLogout();
-  
     }
   }
 
   // Immediately execute fetchData function when script is loaded
   fetchData();
-
 
   // Expose fetchData globally for external use
   window.fetchData = fetchData;
